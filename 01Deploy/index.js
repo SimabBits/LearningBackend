0c11c7d3e3b464db26af6e820391eeab55fbdcdd
Simab <dealwithsimab@gmail.com>
2025-06-24 01:16:01 +0530

    added envoirment var

--- 01Deploy/index.js
+++ 01Deploy/index.js
@@ -1,7 +1,7 @@
+require('dotenv').config()
 const express = require('express') 
 const app = express()
-const port = 3000
-
+const port = 4000
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
@@ -14,7 +14,7 @@ app.get ('/name', (req, res)=>{
     res.send("<h1>Shoaib</h1>");
 })
 
-app.listen(port, () => {
+app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${port}`)
 })
 
