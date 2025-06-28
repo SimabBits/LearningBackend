ef4045c2359e595c2c9772ca772d694ee0be3a34
Simab <dealwithsimab@gmail.com>
2025-06-28 19:03:27 +0530

    updated code

--- Merge/Backend/index.js
+++ Merge/Backend/index.js
@@ -1,13 +1,40 @@
-import express from 'express';
-const app = express()
-const port = process.env.PORT||3000;
-app.get("/home",(req , res)=>{
-    res.send("Home");
-})
+import express from "express";
+const app = express();
+const port = process.env.PORT || 3000;
+app.get("/home", (req, res) => {
+  const jokes = [
+    {
+      id: 1,
+      title: "A joke",
+      content: "This is a joke",
+    },
+    {
+      id: 2,
+      title: "A joke",
+      content: "This is a joke 2",
+    },
+    {
+      id: 3,
+      title: "A joke",
+      content: "This is a joke 3",
+    },
+    {
+      id: 4,
+      title: "A joke",
+      content: "This is a joke 4",
+    },
+    {
+      id: 5,
+      title: "A joke",
+      content: "This is a joke 5",
+    },
+  ];
 
-app.listen (port, ()=>{
-    console.log("Listening at port " + port);
-})
+  res.send(jokes)
+});
 
+app.listen(port, () => {
+  console.log("Listening at port " + port);
+});
 
 console.log("hello");
